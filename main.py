ac90262ce32afec6d065a4c0ec2796d2ec0154b4
Anastasia Taborskaia <nastystax90@mail.ru>
2022-11-24 10:03:13 +0300

    Pic in attachment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 from pprint import pprint
 import vk_api
-# from vk_api import VkUpload
+from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 def get_token(use_group: bool):
@@ -16,11 +16,10 @@ def get_token(use_group: bool):
     token = f.readline()
   return token
 
-# token = get_token(use_group = True)
 vk_user = vk_api.VkApi(token=get_token(use_group = False))
 vk_group = vk_api.VkApi(token=get_token(use_group = True))
 longpoll = VkLongPoll(vk_group)
-# vk_upload = VkUpload(vk_group)
+vk_upload = VkUpload(vk_group)
 users={}
 
 def write_msg(user_id, message, attachment):
@@ -29,15 +28,18 @@ def write_msg(user_id, message, attachment):
                                       'message': message,
                                       'attachment': attachment})
 
-# def write_msg_photo(user_id):
-#     resp = vk_upload.photo_messages(photos = ['1.jpeg', '2.jpeg', '3.jpeg'], peer_id = user_id)[0]
-#     att = list()
-#     for ph in resp:
-#         att.append('photo{}_{}'.format(ph['owner_id'], ph['id']))
-#     vk_group.method('messages.send', {'user_id': user_id,
-#                                       'message': ' - ',
-#                                       'attachment': att,
-#                                       'random_id': randrange(10 ** 7)})
+def write_msg_photo(user_id):
+    with open('1.jpeg', 'wb') as img_file:
+        upload_image = upload.photo_messages(photos = img_file, peer_id = user_id)[0]
+        pic = 'photo{}_{}'.format(upload_image['owner_id'], upload_image['id'])
+    # resp = vk_upload.photo_messages(photos = ['1.jpeg', '2.jpeg', '3.jpeg'], peer_id = user_id)[0]
+    # # att = list()
+    # for ph in resp:
+    #     att.append('photo{}_{}'.format(ph['owner_id'], ph['id']))
+    vk_group.method('messages.send', {'user_id': user_id,
+                                      'message': ' - ',
+                                      'attachment': pic,
+                                      'random_id': randrange(10 ** 7)})
 
 def search_users(sex, age, city, user_id):
     if sex == 1:
@@ -66,14 +68,12 @@ def search_users(sex, age, city, user_id):
         for att in attachment:
             write_msg(user_id, '', att)
 
-
-
 # Сохраняем фото пользователей перед отправкой
-# def save_photo(url_photo):
-#     n = 0
-#     for url in url_photo:
-#         n += 1
-#         urllib.request.urlretrieve(url, f'{str(n)}.jpeg')
+def save_photo(url_photo):
+    n = 0
+    for url in url_photo:
+        n += 1
+        urllib.request.urlretrieve(url, f'{str(n)}.jpeg')
 
 def get_best_photo(id):
     id_photo = []
@@ -105,18 +105,23 @@ for event in longpoll.listen():
                 if len(users.get('bdate', '0')) > 5:
                    users['age'] = age_in_bdate(users['bdate'])
 
-                if users.get('city', 0) == 0:
+                if users.get('city', '0') == '0':
                    message = "Укажите ваш город"
-                elif users.get('age', '0') == 0:
+                   pprint(users)
+                elif users.get('age', '0') == '0':
+                   #  переделать возраст, если дата рождения вида: 11.10 - символов 5, а 27.2 - 4...
+
                    message = "Укажите ваш возраст"
+                   pprint(users)
                 else:
+                   pprint(users)
                    message = "Начинаю поиск партнеров. Подождите пару секунд..."
                    search_users(users['sex'], users['age'], users['city']['id'], event.user_id)
                 write_msg(event.user_id, message, attachment='')
 
             # Получаем недостающие данные
             elif not request == 'hi' and users.get('id') == event.user_id:
-                if users.get('city', 0) == 0:               # Находим город
+                if users.get('city', '0') == '0':               # Находим город
                     cities = vk_user.method('database.getCities', {'country_id': '1', 'q': request})
                     if cities.get('count') == 0:
                        write_msg(event.user_id, "Не могу найти такой город, попробуйте ввести еще раз, либо укажите ближайший крупный город.", '')
