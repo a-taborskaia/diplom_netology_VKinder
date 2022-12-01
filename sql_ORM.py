02afcca1079a7fda0bfe0715fc37890fb6e80468
Anastasia Taborskaia <nastystax90@mail.ru>
2022-12-01 10:54:15 +0300

    Add write functions 2 table - All work

--- sql_ORM.py
+++ sql_ORM.py
@@ -20,63 +20,34 @@ Base = declarative_base()
 metadata = MetaData()
 
 # Создание таблиц:
-class Client(Base):
-    __tablename__ = 'clients'
-    id_client = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False, unique=True)
-    # Объявляется отношение многие ко многим к User через промежуточную таблицу kinders
-    users = relationship("User", secondary='kinders')
-
 class User(Base):
     __tablename__ = 'users'
     id_user = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False, unique=True)
-    # Объявляется отношение многие ко многим к Client через промежуточную таблицу kinders
-    clients = relationship("Client", secondary='kinders')
 
-class Kinder(Base):
-    __tablename__ = 'kinders'
-    # здесь мы объявляем составной ключ, состоящий из двух полей
-    __table_args__ = (PrimaryKeyConstraint('id_client', 'id_user'),)
-    # В промежуточной таблице явно указываются что следующие поля являются внешними ключами
-    id_client = Column(Integer, ForeignKey("clients.id_client"))
+class Search(Base):
+    __tablename__ = 'search'
+    id_search = Column(Integer, primary_key=True)
     id_user = Column(Integer, ForeignKey("users.id_user"))
+    user_search = Column(Integer, nullable=False, unique=True)
 
 # Создание таблиц (выполнили один раз, не стала вствлять в основной код):
 Base.metadata.create_all(engine)
 
-def write_client_id(user_id, session: sessionmaker):
-    # session = Session
-    client = session.query(Client).filter_by(user_id = user_id).scalar()
-    if not client:
-        client = Client(user_id = user_id)
-    session.add(client)
-    session.commit()
-    session.close()
-
-def write_user_id(user_id, session: sessionmaker):
-    # session = Session
-    user = session.query(User).filter_by(user_id = user_id).scalar()
+def write_db(user_id, user_search, session: sessionmaker):
+    not_user = False
+    user = session.query(User).filter_by(user_id=user_id).scalar()
     if not user:
-        user = User(user_id = user_id)
+        user = User(user_id=user_id)
     session.add(user)
+
+    search = session.query(Search).join(User).filter(User.user_id == user_id, Search.user_search == user_search).scalar()
+    if not search:
+        not_user = True
+        search = Search(id_user = user.id_user, user_search = user_search)
+    session.add(search)
     session.commit()
     session.close()
-
-def write_kinder(user_id, client_id, session: sessionmaker):
-    # session = Session
-    # select * from kinders
-    # join users on kinders.id_user = users.id_user
-    # join clients on kinders.id_client = clients.id_client
-    # where users.user_id = 491511729 and clients.user_id = 23034618;
-
-    kinder = session.query(Kinder).join(Client.users).filter(Client.user_id == client_id, User.user_id == user_id)
-    pprint(kinder[0].id_user)
-    if not kinder:
-        kinder = Kinder(id_client = client_id, id_user = user_id)
-    # session.add(kinder)
-    # session.commit()
-    # session.close()
+    return not_user
 
 session = Session()
-write_kinder(user_id=491511729, client_id=23034618, session=session)
